491f307d54e048aa1dff07b00c34a50b2b96297a
Sai Raghu Vamshi Koneti <131893227+raghu071003@users.noreply.github.com>
2024-09-11 16:08:27 +0530

    controllers updated

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,64 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/Cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+    const { fullname, username, email, password } = req.body
+    if (
+        [fullname, email, username, password].some((field) =>
+            field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+    if(existedUser){
+        throw new ApiError(409,"User aldready exists!")
+    }
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // console.log(req.files)
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0){
+        coverImageLocalPath = req.files?.coverImage[0]?.path;
+    }
+    if(!avatarLocalPath){
+        throw new ApiError(400,"avatar is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400,"avatarr is required")
+    }
+
+    const user = await User.create({
+        fullname,
+        avatar:avatar.url,
+        coverImage:coverImage?.url || "",
+        email,
+        password,
+        username:username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500,"Something went wrong while registering a user")
+    }
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"user Created!")
+    )
+
+})
+
+export {
+    registerUser
+}
---
+++ src/index.js
@@ -0,0 +1,67 @@
+// require('dotenv').config({path:'./env'})
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv"
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("Server is running"); 
+    })
+})
+.catch((err) =>{
+    console.log("Mongo DB connection failed",err);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
+// const app = express();
+
+// ;(async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(err)=>{
+//             console.log("Error",err)
+//             throw err
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log('App is listening to port ')
+//         })
+//     }catch(err){
+//         console.error("ERROR" ,err)
+//         throw err
+//     }
+// })()
